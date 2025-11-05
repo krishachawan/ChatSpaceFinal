64d981adcdd0d21ab9c5f1a003ac31738932064c
krishachawan <krishamchawan@gmail.com>
2025-11-05 23:11:22 +0530

    changed the ids for consistency

--- app/src/main/java/com/example/chitchatapp/MainActivity.java
+++ app/src/main/java/com/example/chitchatapp/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.chitchatapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
         titleText = findViewById(R.id.text_app_title);
         nameInput = findViewById(R.id.edit_text_name);
         ipInput = findViewById(R.id.edit_text_ip);
-        hostButton = findViewById(R.id.button_host);
-        joinButton = findViewById(R.id.button_join);
+        hostButton = findViewById(R.id.button_host2);
+        joinButton = findViewById(R.id.button_join2);
 
         chatViewModel = new ViewModelProvider(this).get(ChatViewModel.class);
 
